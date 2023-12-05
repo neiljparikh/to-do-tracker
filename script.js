c2e418e09ce55896340c5f8f186aa618f10b6c86
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2023-12-04 20:09:26 -0500

    Added blocks and labels for each required timeslot

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
 $(function () {
-
+  var currentDay = dayjs().format('dddd');
+  var dateContainer = document.getElementById('current-day');
   
-  targetElement.addEventListener('click', function(event) {
-    // Your code to be executed when the element is clicked
-    console.log('Element clicked!');
-    console.log('Event details:', event);
-  });
+  dateContainer.textContent = 'Today is ' + currentDay;
+
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -25,4 +24,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
